b370c9f657fd2fb4cbd16d212510d2bca2255fc7
daltonsilverman <daltonsilverman04@gmail.com>
2025-03-17 19:16:55 -0700

    framebuffer, axes, and scaling implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+const int WIDTH = 1280;
+const int HEIGHT = 640;
+const int XSCALE = 16;
+const int YSCALE = 16;
+
+void putPixel(int x, int y, uint32_t color, uint32_t* buffer){
+    if(x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) return;
+    buffer[y * WIDTH + x] = color;
+}
+
+void drawGridLines(uint32_t* buffer){
+    for (int x = 0; x < WIDTH; x += WIDTH / XSCALE){
+        for (int y = 0; y < HEIGHT; ++y) {
+            putPixel(x, y, 0xFF00FF00, buffer);
+        }
+    }
+
+    for (int y = 0; y < HEIGHT; y += HEIGHT / YSCALE){
+        for (int x = 0; x < WIDTH; ++x){
+            putPixel(x, y, 0xFF00FF00, buffer);
+        }
+    }
+}
+
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
@@ -10,7 +34,7 @@ int main() {
     SDL_Window* window = SDL_CreateWindow("Software Renderer",
                                             SDL_WINDOWPOS_CENTERED,
                                             SDL_WINDOWPOS_CENTERED,
-                                            800, 600,
+                                            WIDTH, HEIGHT,
                                             SDL_WINDOW_SHOWN);
 
     if (!window) {
@@ -30,20 +54,44 @@ int main() {
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
+    SDL_Texture* texture = SDL_CreateTexture(renderer, 
+                                             SDL_PIXELFORMAT_ARGB8888,
+                                             SDL_TEXTUREACCESS_STREAMING,
+                                             WIDTH, HEIGHT);
+
+    uint32_t* framebuffer = new uint32_t[WIDTH * HEIGHT];
+
+    drawGridLines(framebuffer);
+
+    for(int y = 0; y < HEIGHT; ++y) {
+        putPixel(WIDTH/2, y, 0xFFFF0000, framebuffer);
+    }
+
+    for(int x = 0; x < WIDTH; ++x) {
+        putPixel(x, HEIGHT/2, 0xFFFF0000, framebuffer);
+    }
+
+
     bool running = true;
     Uint32 startTime = SDL_GetTicks();
     SDL_Event event;
-    while (running && (SDL_GetTicks() - startTime < 2000)) {
+    while (running && (SDL_GetTicks() - startTime < 10000)) {
             while (SDL_PollEvent(&event)) {
                 if (event.type == SDL_QUIT) {
                     running = false;
                 }
             }
+
+            SDL_UpdateTexture(texture, nullptr, framebuffer, WIDTH * sizeof(uint32_t));
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+            SDL_RenderPresent(renderer);
             SDL_Delay(16); //60 FPS idle loop to prevent pegging
         }
 
 
-
+    delete[] framebuffer;
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
